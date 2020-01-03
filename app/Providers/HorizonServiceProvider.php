38f8781db296dd02493bdae5d630a71a44a6c700
Kai Sassnowski <sassnowski@wycomco.de>
2020-01-03 17:00:31 +0100

    Install horizon

---
+++ app/Providers/HorizonServiceProvider.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+namespace App\Providers;
+
+use Laravel\Horizon\Horizon;
+use Illuminate\Support\Facades\Gate;
+use Laravel\Horizon\HorizonApplicationServiceProvider;
+
+class HorizonServiceProvider extends HorizonApplicationServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        Horizon::night();
+    }
+
+    /**
+     * Register the Horizon gate.
+     *
+     * This gate determines who can access Horizon in non-local environments.
+     *
+     * @return void
+     */
+    protected function gate()
+    {
+        Gate::define('viewHorizon', function ($user) {
+            return in_array($user->email, [
+                'me@kai-sassnowski.com',
+            ]);
+        });
+    }
+}
