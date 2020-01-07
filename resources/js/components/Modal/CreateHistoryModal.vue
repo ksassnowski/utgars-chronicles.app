1d077224d73b369cd9c91b782327eafa72b4e25f
Kai Sassnowski <sassnowski@wycomco.de>
2020-01-07 18:43:53 +0100

    Add helptext to create history modal

--- resources/js/components/Modal/CreateHistoryModal.vue
+++ resources/js/components/Modal/CreateHistoryModal.vue
@@ -4,6 +4,7 @@
             <div class="mb-4">
                 <label for="name" class="label">Name</label>
                 <input type="text" class="input" id="name" v-model="form.name" ref="input" required>
+                <small class="text-xs text-gray-600">This is the seed of your history. For now you can simply put in a placeholder and change it in-game.</small>
                 <small v-if="$page.errors.name" class="text-red-600 text-xs">{{ $page.errors.name[0] }}</small>
             </div>
 
