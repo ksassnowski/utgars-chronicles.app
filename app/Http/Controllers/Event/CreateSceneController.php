97edc5f911bd7b9c456a65720e0c57a3f45ea0f2
Kai Sassnowski <me@kai-sassnowski.com>
2020-05-17 15:44:56 +0200

    Clean up obsolete code

--- app/Http/Controllers/Event/CreateSceneController.php
+++ app/Http/Controllers/Event/CreateSceneController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Event;
 
 use App\Event;
-use App\Scene;
 use App\Events\BoardUpdated;
 use Illuminate\Http\JsonResponse;
 use App\Http\Requests\History\CreateSceneRequest;
@@ -12,8 +11,7 @@ final class CreateSceneController
 {
     public function __invoke(CreateSceneRequest $request, Event $event): JsonResponse
     {
-        /** @var Scene $scene */
-        $scene = $event->scenes()->create($request->validated());
+        $event->scenes()->create($request->validated());
 
         broadcast(new BoardUpdated($event->period->history));
 
