7584ac7399fba0d64bf46ea87eeb5c30eb5f7d14
Kai Sassnowski <me@kai-sassnowski.com>
2020-11-14 17:59:28 +0100

    Add better scaling for game board on large screens

--- resources/js/components/PeriodCard.vue
+++ resources/js/components/PeriodCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-1/2 md:w-1/4 lg:w-1/5 flex-shrink-0">
+    <div class="w-1/2 md:w-1/4 lg:w-1/5 2xl:w-1/6 3xl:w-1/7 flex-shrink-0">
         <CreateEventModal
             v-if="showModal"
             :period="period"
@@ -9,14 +9,19 @@
         />
 
         <div class="px-4 game-card">
-            <article class="p-8 rounded-lg border border-gray-200 mb-6 shadow-lg bg-white relative panzoom-exclude group">
+            <article
+                class="p-8 rounded-lg border border-gray-200 mb-6 shadow-lg bg-white relative panzoom-exclude group"
+            >
                 <template v-if="!editing">
                     <button
                         title="Add period before this one"
                         class="game-add-button left"
                         @click="$emit('insertPeriod', period.position)"
                     >
-                        <Icon name="add-solid" class="fill-current text-gray-500 hover:text-indigo-700 w-6" />
+                        <Icon
+                            name="add-solid"
+                            class="fill-current text-gray-500 hover:text-indigo-700 w-6"
+                        />
                     </button>
 
                     <button
@@ -24,16 +29,25 @@
                         class="game-add-button right"
                         @click="$emit('insertPeriod', period.position + 1)"
                     >
-                        <Icon name="add-solid" class="fill-current text-gray-500 hover:text-indigo-700 w-6" />
+                        <Icon
+                            name="add-solid"
+                            class="fill-current text-gray-500 hover:text-indigo-700 w-6"
+                        />
                     </button>
 
-                    <div class="invisible group-hover:visible absolute left-0 top-0 w-full pl-3 pr-2 pt-2 flex justify-between z-20">
+                    <div
+                        class="invisible group-hover:visible absolute left-0 top-0 w-full pl-3 pr-2 pt-2 flex justify-between z-20"
+                    >
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             class="handle w-4 h-4 fill-current text-gray-400 cursor-move"
                             style="margin-top: 2px"
                             viewBox="0 0 20 20"
-                        ><path d="M0 3h20v2H0V3zm0 4h20v2H0V7zm0 4h20v2H0v-2zm0 4h20v2H0v-2z"/></svg>
+                        >
+                            <path
+                                d="M0 3h20v2H0V3zm0 4h20v2H0V7zm0 4h20v2H0v-2zm0 4h20v2H0v-2z"
+                            />
+                        </svg>
 
                         <SettingsPanel
                             v-if="!editing"
@@ -42,12 +56,17 @@
                         />
                     </div>
 
-                    <h3 class="font-bold tracking-wide text-center py-2">{{ period.name }}</h3>
+                    <h3 class="font-bold tracking-wide text-center py-2">
+                        {{ period.name }}
+                    </h3>
 
                     <div
                         class="rounded-full w-6 h-6 border-2 border-gray-800 absolute"
                         style="top: -12px;"
-                        :class="{ 'bg-white': period.type === 'light', 'bg-gray-800': period.type === 'dark' }"
+                        :class="{
+                            'bg-white': period.type === 'light',
+                            'bg-gray-800': period.type === 'dark'
+                        }"
                     ></div>
 
                     <p
@@ -57,18 +76,29 @@
                         Period
                     </p>
 
-                    <div class="flex justify-end absolute inset-x-0 bottom-0 p-2 invisible group-hover:visible">
+                    <div
+                        class="flex justify-end absolute inset-x-0 bottom-0 p-2 invisible group-hover:visible"
+                    >
                         <button
                             class="text-indigo-700 text-sm"
-                            @click="() => openCreateEventModal(nextEventPosition)"
-                        >Add Event</button>
+                            @click="
+                                () => openCreateEventModal(nextEventPosition)
+                            "
+                        >
+                            Add Event
+                        </button>
                     </div>
                 </template>
 
                 <form v-else @submit.prevent="submit">
                     <div class="mb-4">
                         <label for="name" class="label">Name</label>
-                        <textarea type="text" id="name" v-model="form.name" class="input"></textarea>
+                        <textarea
+                            type="text"
+                            id="name"
+                            v-model="form.name"
+                            class="input"
+                        ></textarea>
                     </div>
 
                     <div class="mb-4">
@@ -77,26 +107,47 @@
                         <div class="flex justify-between items-center">
                             <div>
                                 <label for="dark" class="label">Dark</label>
-                                <input type="radio" id="dark" value="dark" v-model="form.type">
+                                <input
+                                    type="radio"
+                                    id="dark"
+                                    value="dark"
+                                    v-model="form.type"
+                                />
                             </div>
 
                             <div>
                                 <label for="light" class="label">Light</label>
-                                <input type="radio" id="light" value="light" v-model="form.type">
+                                <input
+                                    type="radio"
+                                    id="light"
+                                    value="light"
+                                    v-model="form.type"
+                                />
                             </div>
                         </div>
                     </div>
 
                     <LoadingButton :loading="loading">
-                        {{ loading ? 'Hang on...' : 'Save' }}
+                        {{ loading ? "Hang on..." : "Save" }}
                     </LoadingButton>
 
-                    <button type="button" class="text-sm text-gray-700 mt-2 text-center w-full" @click="cancel">Cancel</button>
+                    <button
+                        type="button"
+                        class="text-sm text-gray-700 mt-2 text-center w-full"
+                        @click="cancel"
+                    >
+                        Cancel
+                    </button>
                 </form>
             </article>
         </div>
 
-        <draggable :list="orderedEvents" @change="eventMoved" class="flex flex-col px-4" handle=".handle">
+        <draggable
+            :list="orderedEvents"
+            @change="eventMoved"
+            class="flex flex-col px-4"
+            handle=".handle"
+        >
             <EventCard
                 v-for="event in orderedEvents"
                 :key="event.id"
@@ -109,21 +160,21 @@
 </template>
 
 <script>
-import axios from 'axios';
-import draggable from 'vuedraggable';
-import sortBy from 'lodash/sortBy';
+import axios from "axios";
+import draggable from "vuedraggable";
+import sortBy from "lodash/sortBy";
 
-import EventCard from './EventCard';
-import SettingsPanel from './SettingsPanel';
-import Modal from './Modal';
-import CreateEventModal from './Modal/CreateEventModal';
+import EventCard from "./EventCard";
+import SettingsPanel from "./SettingsPanel";
+import Modal from "./Modal";
+import CreateEventModal from "./Modal/CreateEventModal";
 import LoadingButton from "./LoadingButton";
 import Icon from "./Icon";
 
 export default {
-    name: 'PeriodCard',
+    name: "PeriodCard",
 
-    props: ['period', 'historyId'],
+    props: ["period", "historyId"],
 
     components: {
         Icon,
@@ -132,7 +183,7 @@ export default {
         Modal,
         SettingsPanel,
         draggable,
-        EventCard,
+        EventCard
     },
 
     computed: {
@@ -147,7 +198,7 @@ export default {
         },
 
         orderedEvents() {
-            return sortBy(this.period.events, ['position']);
+            return sortBy(this.period.events, ["position"]);
         }
     },
 
@@ -159,8 +210,8 @@ export default {
             newEventPosition: this.nextEventPosition,
             form: {
                 name: this.period.name,
-                type: this.period.type,
-            },
+                type: this.period.type
+            }
         };
     },
 
@@ -170,11 +221,11 @@ export default {
                 return;
             }
 
-            Bus.$emit('event.moved', {
+            Bus.$emit("event.moved", {
                 period: this.period,
                 event: e.moved.element,
-                position: e.moved.newIndex + 1,
-            })
+                position: e.moved.newIndex + 1
+            });
         },
 
         edit() {
@@ -191,8 +242,8 @@ export default {
 
         submit() {
             if (
-                this.form.name === this.period.name
-                && this.form.type === this.period.type
+                this.form.name === this.period.name &&
+                this.form.type === this.period.type
             ) {
                 this.editing = false;
                 return;
@@ -200,7 +251,14 @@ export default {
 
             this.loading = true;
 
-            axios.put(this.$route('periods.update', [this.historyId, this.period]), this.form)
+            axios
+                .put(
+                    this.$route("periods.update", [
+                        this.historyId,
+                        this.period
+                    ]),
+                    this.form
+                )
                 .then(() => {
                     this.loading = false;
                     this.editing = false;
@@ -211,10 +269,18 @@ export default {
         },
 
         remove() {
-            const confirmed = confirm('Really delete this period? This will delete all events and scenes as well!');
+            const confirmed = confirm(
+                "Really delete this period? This will delete all events and scenes as well!"
+            );
 
             if (confirmed) {
-                axios.delete(this.$route('periods.delete', [this.historyId, this.period]))
+                axios
+                    .delete(
+                        this.$route("periods.delete", [
+                            this.historyId,
+                            this.period
+                        ])
+                    )
                     .catch(console.error);
             }
         },
@@ -222,7 +288,7 @@ export default {
         openCreateEventModal(position) {
             this.newEventPosition = position;
             this.showModal = true;
-        },
-    },
+        }
+    }
 };
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
     },
     theme: {
         extend: {
+            screens: {
+                "2xl": "1700px",
+                "3xl": "2040px"
+            },
+            width: {
+                "1/7": "14.2857143%"
+            },
             fontFamily: {
                 sans: ["Inter var", ...defaultTheme.fontFamily.sans]
             }
