23b8c37c56071c49b437a873ebaadc114524b72a
Kai Sassnowski <me@kai-sassnowski.com>
2020-11-14 17:41:51 +0100

    Ensure consistent order in player list (#7)

--- resources/js/components/PlayerList.vue
+++ resources/js/components/PlayerList.vue
@@ -3,38 +3,47 @@
         <h3 class="font-bold text-sm text-gray-700 mb-4">Players</h3>
         <ul>
             <li
-                v-for="player in players" :key="player.id"
+                v-for="player in sortedPlayers"
+                :key="player.id"
                 class="bg-white px-2 py-1 border-l-4 border-green-400 mb-2 text-sm shadow"
-            >{{ player.name }}</li>
+            >
+                {{ player.name }}
+            </li>
         </ul>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'PlayerList',
+import { sortBy } from "lodash";
 
-        props: ['channel'],
+export default {
+    name: "PlayerList",
 
-        data() {
-            return {
-                players: [],
-                colors: [
-                ],
-            };
-        },
+    props: ["channel"],
 
-        created() {
-            Echo.join(this.channel)
-                .here((users) => {
-                    this.players = users;
-                })
-                .joining((user) => {
-                    this.players.push(user);
-                })
-                .leaving((user) => {
-                    this.players = this.players.filter(p => p.id !== user.id);
-                });
-        },
+    computed: {
+        sortedPlayers() {
+            return sortBy(this.players, "name");
+        }
+    },
+
+    data() {
+        return {
+            players: []
+        };
+    },
+
+    created() {
+        Echo.join(this.channel)
+            .here(users => {
+                this.players = users;
+            })
+            .joining(user => {
+                this.players.push(user);
+            })
+            .leaving(user => {
+                this.players = this.players.filter(p => p.id !== user.id);
+            });
     }
+};
 </script>
