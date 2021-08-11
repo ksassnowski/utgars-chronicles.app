b353de6ccd4285a84d27b35bc6f48809195e25ba
Kai Sassnowski <me@kai-sassnowski.com>
2021-08-11 12:50:35 +0200

    Fix word wrapping in cards

--- resources/js/components/EventCard.vue
+++ resources/js/components/EventCard.vue
@@ -1,16 +1,57 @@
 <template>
     <div class="relative game-card">
         <article
-            class="relative p-8 relative shadow-sm rounded-lg border border-gray-200 text-sm w-full min-h-32 group"
-            :class="{ 'bg-gray-700 text-white': event.type === 'dark', 'bg-white text-gray-700': event.type === 'light' }"
+            class="
+                relative
+                p-8
+                relative
+                shadow-sm
+                rounded-lg
+                border border-gray-200
+                text-sm
+                w-full
+                min-h-32
+                group
+            "
+            :class="{
+                'bg-gray-700 text-white': event.type === 'dark',
+                'bg-white text-gray-700': event.type === 'light',
+            }"
         >
             <template v-if="!editing">
-                <div class="invisible group-hover:visible absolute left-0 top-0 w-full pl-3 pr-2 pt-2 flex justify-between z-20">
+                <div
+                    class="
+                        invisible
+                        group-hover:visible
+                        absolute
+                        left-0
+                        top-0
+                        w-full
+                        pl-3
+                        pr-2
+                        pt-2
+                        flex
+                        justify-between
+                        z-20
+                    "
+                >
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
-                        class="handle w-4 h-4 fill-current text-gray-400 cursor-move mt-[2px]"
+                        class="
+                            handle
+                            w-4
+                            h-4
+                            fill-current
+                            text-gray-400
+                            cursor-move
+                            mt-[2px]
+                        "
                         viewBox="0 0 20 20"
-                    ><path d="M0 3h20v2H0V3zm0 4h20v2H0V7zm0 4h20v2H0v-2zm0 4h20v2H0v-2z"/></svg>
+                    >
+                        <path
+                            d="M0 3h20v2H0V3zm0 4h20v2H0V7zm0 4h20v2H0v-2zm0 4h20v2H0v-2z"
+                        />
+                    </svg>
 
                     <SettingsPanel
                         v-if="!editing"
@@ -19,21 +60,50 @@
                     />
                 </div>
 
-                <h4 class="text-center">{{ event.name }}</h4>
+                <h4 class="text-center whitespace-normal">{{ event.name }}</h4>
 
-                <div class="absolute invisible group-hover:visible flex justify-end items-center inset-x-0 bottom-0 px-2 pb-2">
+                <div
+                    class="
+                        absolute
+                        invisible
+                        group-hover:visible
+                        flex
+                        justify-end
+                        items-center
+                        inset-x-0
+                        bottom-0
+                        px-2
+                        pb-2
+                    "
+                >
                     <SceneModal :event="event">
                         <button
                             class="text-sm"
-                            :class="{ 'text-indigo-700': event.type === 'light', 'text-indigo-300': event.type === 'dark' }"
-                        >Add Scene</button>
+                            :class="{
+                                'text-indigo-700': event.type === 'light',
+                                'text-indigo-300': event.type === 'dark',
+                            }"
+                        >
+                            Add Scene
+                        </button>
                     </SceneModal>
                 </div>
 
                 <p
-                    class="absolute top-0 text-sm font-bold leading-loose uppercase px-1"
-                    :class="{ 'text-white bg-gray-700': event.type === 'dark', 'text-gray-700 bg-white': event.type === 'light' }"
-                    style="top: -15px; right: 20px;"
+                    class="
+                        absolute
+                        top-0
+                        text-sm
+                        font-bold
+                        leading-loose
+                        uppercase
+                        px-1
+                    "
+                    :class="{
+                        'text-white bg-gray-700': event.type === 'dark',
+                        'text-gray-700 bg-white': event.type === 'light',
+                    }"
+                    style="top: -15px; right: 20px"
                 >
                     Event
                 </p>
@@ -42,7 +112,12 @@
             <form v-else @submit.prevent="submit">
                 <div class="mb-4">
                     <label for="name" class="label">Name</label>
-                    <textarea id="name" rows="4" class="input" v-model="form.name"></textarea>
+                    <textarea
+                        id="name"
+                        rows="4"
+                        class="input"
+                        v-model="form.name"
+                    ></textarea>
                 </div>
 
                 <div class="mb-4">
@@ -50,23 +125,36 @@
 
                     <div class="flex justify-between items-center">
                         <div>
-                            <input type="radio" id="dark" value="dark" v-model="form.type">
+                            <input
+                                type="radio"
+                                id="dark"
+                                value="dark"
+                                v-model="form.type"
+                            />
                             <label for="dark">Dark</label>
                         </div>
 
                         <div>
-                            <input type="radio" id="light" value="light" v-model="form.type">
+                            <input
+                                type="radio"
+                                id="light"
+                                value="light"
+                                v-model="form.type"
+                            />
                             <label for="light">Light</label>
                         </div>
                     </div>
                 </div>
 
+                <LoadingButton :loading="form.processing"> Save </LoadingButton>
 
-                <LoadingButton :loading="form.processing">
-                    Save
-                </LoadingButton>
-
-                <button type="button" class="w-full text-gray-700 mt-2" @click="cancel">Cancel</button>
+                <button
+                    type="button"
+                    class="w-full text-gray-700 mt-2"
+                    @click="cancel"
+                >
+                    Cancel
+                </button>
             </form>
         </article>
 
@@ -76,18 +164,17 @@
             handle=".handle"
             item-key="id"
         >
-            <template #item="{element}">
+            <template #item="{ element }">
                 <SceneCard :scene="element" />
             </template>
         </draggable>
-
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, toRefs } from "vue";
 import { useForm } from "@inertiajs/inertia-vue3";
-import draggable from 'vuedraggable';
+import draggable from "vuedraggable";
 
 import LoadingButton from "./LoadingButton.vue";
 import SettingsPanel from "./SettingsPanel.vue";
@@ -96,14 +183,14 @@ import SceneModal from "./Modal/SceneModal.vue";
 import Icon from "./Icon.vue";
 
 export default defineComponent({
-    name: 'EventCard',
+    name: "EventCard",
 
     props: {
         event: Object,
         period: Object,
     },
 
-    inject: ['history'],
+    inject: ["history"],
 
     components: {
         Icon,
@@ -133,29 +220,34 @@ export default defineComponent({
             );
         },
 
-        submit()  {
+        submit() {
             if (
-                this.form.type === this.event.type
-                && this.form.name === this.event.name
+                this.form.type === this.event.type &&
+                this.form.name === this.event.name
             ) {
                 return this.stopEditing();
             }
 
-            this.form.put(this.$route('events.update', [this.history, this.event]), {
-                only: ["history"],
-                onSuccess: this.stopEditing
-            });
+            this.form.put(
+                this.$route("events.update", [this.history, this.event]),
+                {
+                    only: ["history"],
+                    onSuccess: this.stopEditing,
+                }
+            );
         },
 
         remove() {
-            const confirmed = confirm('Really delete this event? All scenes belonging to this event will be deleted too!');
+            const confirmed = confirm(
+                "Really delete this event? All scenes belonging to this event will be deleted too!"
+            );
 
             if (!confirmed) {
                 return;
             }
 
             this.$inertia.delete(
-                this.$route('events.delete', [this.history, this.event]),
+                this.$route("events.delete", [this.history, this.event]),
                 { only: ["history"] }
             );
         },
@@ -169,7 +261,7 @@ export default defineComponent({
         const resetForm = () => {
             form.type = props.event.type;
             form.name = props.event.name;
-        }
+        };
         const editing = ref(false);
         const startEditing = () => {
             resetForm();
@@ -178,9 +270,9 @@ export default defineComponent({
         const stopEditing = () => {
             resetForm();
             editing.value = false;
-        }
+        };
 
         return { form, startEditing, stopEditing, editing };
-    }
+    },
 });
 </script>
