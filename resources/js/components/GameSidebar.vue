79ca2640d3235593000e38191ac90722babb295c
Kai Sassnowski <me@kai-sassnowski.com>
2021-08-11 21:31:27 +0200

    Add poor man's pub-sub implementation to toggle all scenes at once

--- resources/js/components/GameSidebar.vue
+++ resources/js/components/GameSidebar.vue
@@ -36,26 +36,33 @@
                 <LegacyTracker :history="history" :legacies="legacies" />
             </div>
 
-            <Link
-                :href="$route('home')"
-                class="
-                    flex
-                    items-center
-                    sm:w-full
-                    bg-indigo-800
-                    sm:bg-indigo-900
-                    text-indigo-100
-                    font-medium
-                    text-xs
-                    px-4
-                    py-4
-                    justify-center
-                    hover:text-white hover:bg-black hover:bg-opacity-30
-                "
-            >
-                <ChevronDoubleLeftIcon class="w-3 h-3 mr-1" />
-                back
-            </Link>
+            <div>
+                <!--button @click="toggleScenes" class="text-white text-sm">
+                    Toggle Scenes
+                </button-->
+
+                <Link
+                    :href="$route('home')"
+                    class="
+                        flex
+                        items-center
+                        sm:w-full
+                        bg-indigo-800
+                        sm:bg-indigo-900
+                        text-indigo-100
+                        font-medium
+                        text-xs
+                        px-4
+                        py-4
+                        mt-2
+                        justify-center
+                        hover:text-white hover:bg-black hover:bg-opacity-30
+                    "
+                >
+                    <ChevronDoubleLeftIcon class="w-3 h-3 mr-1" />
+                    back
+                </Link>
+            </div>
         </div>
     </div>
 </template>
@@ -65,6 +72,7 @@ import { defineComponent } from "vue";
 import { Link } from "@inertiajs/inertia-vue3";
 import { ChevronDoubleLeftIcon } from "@heroicons/vue/outline";
 
+import { useEmitter } from "../composables/useEmitter";
 import LegacyTracker from "./LegacyTracker.vue";
 import Palette from "./Palette.vue";
 import FocusTracker from "./FocusTracker.vue";
@@ -85,6 +93,13 @@ export default defineComponent({
         FocusTracker,
         Link,
         ChevronDoubleLeftIcon,
+    },
+
+    setup() {
+        const emitter = useEmitter();
+        const toggleScenes = () => emitter.trigger("scenes:toggle");
+
+        return { toggleScenes };
     },
 });
 </script>
