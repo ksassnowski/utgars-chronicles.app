248218fa20530b595446358893859075673acf59
Kai Sassnowski <me@kai-sassnowski.com>
2022-08-03 13:37:39 +0200

    Styling changes to inviation form

--- resources/scripts/Pages/Game/InvitationForm.vue
+++ resources/scripts/Pages/Game/InvitationForm.vue
@@ -1,83 +1,65 @@
 <template>
-    <div class="flex justify-center items-center h-full game-container px-4">
-        <form
-            @submit.prevent="submit"
-            class="w-full sm:w-2/3 lg:w-2/5 bg-white rounded border border-gray-200 p-6 shadow-xl"
-        >
-            <p class="text-2xl mb-4 text-gray-800 text-center">
-                <strong>{{ inviteeName }}</strong> has invited you to join their
-                game of Microscope.
-            </p>
+    <Head title="Join game" />
 
-            <div class="mb-4">
-                <label for="name" class="label">Name</label>
-                <input
-                    type="text"
-                    class="input"
-                    id="name"
-                    v-model="form.name"
-                    autofocus
-                    required
-                />
-                <small class="text-xs text-gray-600"
-                    >This is the name that will show up to the other players
-                    in-game.</small
-                >
-            </div>
+    <div class="container mx-auto px-4 h-full flex flex-col space-y-4 items-center justify-center">
+        <Link href="/" class="text-2xl font-bold tracking-tight text-gray-700">Utgar's Chronicles</Link>
 
-            <div class="flex justify-end">
-                <button
-                    class="bg-indigo-700 text-white font-bold px-6 py-3 rounded disabled:opacity-50 disabled:cursor-not-allowed"
-                    :disabled="!formValid || form.processing"
-                >
-                    Join Game
-                </button>
-            </div>
-        </form>
+        <Panel class="max-w-md">
+            <form @submit.prevent="submit">
+                <p class="text-2xl mb-4 text-gray-800 text-center">
+                    <strong>{{ inviteeName }}</strong> has invited you to join their
+                    game of Microscope.
+                </p>
+
+                <div class="mb-4">
+                    <label for="name" class="label">Name</label>
+                    <input
+                        type="text"
+                        class="input"
+                        id="name"
+                        v-model="form.name"
+                        autofocus
+                        required
+                    />
+                    <small class="text-xs text-gray-600"
+                        >This is the name that will show up to the other players
+                        in-game.</small
+                    >
+                </div>
+
+                <div class="flex justify-end">
+                    <LoadingButton :loading="form.processing">
+                        Join Game
+                    </LoadingButton>
+                </div>
+            </form>
+        </Panel>
     </div>
 </template>
 
-<script>
-export default {
-    name: "InvitationForm",
+<script lang="ts" setup>
+import { computed } from "vue";
+import { Link, Head, useForm } from "@inertiajs/inertia-vue3";
 
-    props: {
-        acceptUrl: {
-            type: String,
-            required: true
-        },
-        inviteeName: {
-            type: String,
-            required: true
-        }
-    },
+import Panel from "@/components/UI/Panel.vue";
+import LoadingButton from "../../components/LoadingButton.vue";
 
-    data() {
-        return {
-            form: this.$inertia.form({
-                name: null
-            })
-        };
-    },
+const props = defineProps<{ acceptUrl: string, inviteeName: string }>();
 
-    computed: {
-        formValid() {
-            return this.form.name !== null;
-        }
-    },
+const form = useForm({
+    name: null
+});
+const formValid = computed(() => form.name !== null);
 
-    methods: {
-        submit() {
-            if (this.formValid) {
-                this.form.post(this.acceptUrl, {
-                    onSuccess: () => {
-                        if (window.fathom) {
-                            window.fathom.trackGoal("UKDSEW8G", 0);
-                        }
-                    }
-                });
+const submit = () => {
+    if (formValid.value) {
+        this.form.post(props.acceptUrl, {
+            onSuccess: () => {
+                if (window.fathom) {
+                    window.fathom.trackGoal("UKDSEW8G", 0);
+                }
             }
-        }
+        });
     }
 };
 </script>
