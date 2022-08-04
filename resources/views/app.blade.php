31cfc89aee59e54716a7dd24c367b126a1b0a1d4
Kai Sassnowski <me@kai-sassnowski.com>
2022-08-04 21:26:22 +0200

    wip

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -20,7 +20,7 @@
         <script src="https://cdn.usefathom.com/script.js" data-site="RHQXBEGX" included-domains="utgars-chronicles.app" defer></script>
     @endproduction
 
-    @vite(['resources/scripts/app.ts'])
+    @vite('resources/scripts/app.ts')
 
     @routes
 </head>
