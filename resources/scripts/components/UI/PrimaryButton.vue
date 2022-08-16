8410d6ed1a93cd9ab0a7b817c67a6a5118ec274f
Kai Sassnowski <me@kai-sassnowski.com>
2022-08-16 05:54:13 +0200

    Fix PrimaryButton not rendering as link

--- resources/scripts/components/UI/PrimaryButton.vue
+++ resources/scripts/components/UI/PrimaryButton.vue
@@ -1,15 +1,24 @@
 <template>
-    <component
-        :is="as"
-        class="bg-indigo-700 text-white rounded-md text-sm font-medium px-4 py-2 transition duration-300 hover:bg-indigo-600"
+    <Link
+        v-if="href"
+        :href="href"
+        :class="classes"
     >
         <slot />
-    </component>
+    </Link>
+
+    <button v-else :class="classes">
+        <slot />
+    </button>
 </template>
 
 <script lang="ts" setup>
+import { Link } from "@inertiajs/inertia-vue3";
+
 withDefaults(
-    defineProps<{ as?: string }>(),
-    { as: "button" },
+    defineProps<{ href?: string|null }>(),
+    { href: null },
 );
+
+const classes = "bg-indigo-700 text-white rounded-md text-sm font-medium px-4 py-2 transition duration-300 hover:bg-indigo-600";
 </script>
